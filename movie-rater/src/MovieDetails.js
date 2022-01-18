54587a4b668f576757118e12e83953d5c02a8758
virabu <ranga.viki@gmail.com>
2022-01-18 11:49:28 +0100

    MovieDetails updated

--- movie-rater/src/MovieDetails.js
+++ movie-rater/src/MovieDetails.js
@@ -10,9 +10,25 @@ const MovieDetails = () => {
       { isPending && <div>Loading...</div> }
       { error && <div>{ error }</div> }
       { movie && (
-        <div>
-          <h3>{ movie.name }</h3>
-          <p></p>
+        <div className="MovieDetails">
+          <img className="image" src={ movie.imgUrl } alt="moviePic"/>
+          <div className="MovieDetailsTitle">
+            <h1>{ movie.name }</h1>
+            <h2>{ movie.year }</h2>
+          </div>
+          <div className="MovieDetailsInfo">
+            <p><strong>Director:</strong> { movie.director }</p>
+            <p><strong>Stars:</strong> { movie.stars }</p>
+            <p><strong>Writers:</strong> { movie.writers }</p>
+            <p><strong>Review:</strong> { movie.review }</p>
+            <p><strong>Directing:</strong> { movie.ratings.directing } /5</p>
+            <p><strong>Acting:</strong> { movie.ratings.acting } /5</p>
+            <p><strong>Costume design:</strong> { movie.ratings.costumeDesign } /5</p>
+            <p><strong>Editing:</strong> { movie.ratings.editing } /5</p>
+            <p><strong>Music:</strong> { movie.ratings.music } /5</p>
+            <p><strong>Visual effects:</strong> { movie.ratings.visualEffects } /5</p>
+            <p><strong>Screenplay:</strong> { movie.ratings.screenplay } /5</p>
+          </div>
         </div>
       )}
     </div>
--- movie-rater/src/MovieList.js
+++ movie-rater/src/MovieList.js
@@ -7,7 +7,7 @@ const MovieList = ({movies, name}) => {
       {movies.map((movie) => (
         <div key={movie.id}>
           <Link to={`/movies/${movie.id}`}>
-            <img src={ movie.imgUrl }/>
+            <img className="image" src={ movie.imgUrl } alt="moviePic"/>
             <p>{ movie.name }</p>
             <p>{ movie.year }</p>
           </Link>
